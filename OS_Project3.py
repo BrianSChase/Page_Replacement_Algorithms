fc2424ecf8bdad4b481da3d106906d3e78154e0e
BrianSChase <46979653+BrianSChase@users.noreply.github.com>
2020-11-24 19:22:34 -0600

    Add files via upload

--- OS_Project3.py
+++ OS_Project3.py
@@ -4,7 +4,74 @@ Created on Wed Nov 11 20:08:40 2020
 
 @author: Brian
 """
-
+"""
+# Python3 implementation of FIFO page 
+# replacement in Operating Systems. 
+from queue import Queue  
+  
+# Function to find page faults using FIFO  
+def pageFaults(pages, n, capacity): 
+      
+    # To represent set of current pages.  
+    # We use an unordered_set so that we 
+    # quickly check if a page is present 
+    # in set or not  
+    s = set()  
+  
+    # To store the pages in FIFO manner  
+    indexes = Queue()  
+  
+    # Start from initial page  
+    page_faults = 0
+    for i in range(n): 
+          
+        # Check if the set can hold  
+        # more pages  
+        if (len(s) < capacity): 
+              
+            # Insert it into set if not present  
+            # already which represents page fault  
+            if (pages[i] not in s): 
+                s.add(pages[i])  
+  
+                # increment page fault  
+                page_faults += 1
+  
+                # Push the current page into 
+                # the queue  
+                indexes.put(pages[i]) 
+  
+        # If the set is full then need to perform FIFO  
+        # i.e. remove the first page of the queue from  
+        # set and queue both and insert the current page  
+        else: 
+              
+            # Check if current page is not  
+            # already present in the set  
+            if (pages[i] not in s): 
+                  
+                # Pop the first page from the queue  
+                val = indexes.queue[0]  
+  
+                indexes.get()  
+  
+                # Remove the indexes page  
+                s.remove(val)  
+  
+                # insert the current page  
+                s.add(pages[i])  
+  
+                # push the current page into  
+                # the queue  
+                indexes.put(pages[i])  
+  
+                # Increment page faults  
+                page_faults += 1
+  
+    return page_faults 
+    
+    
+"""  
         
 def FCFS(pages, cap):    
     frames = []
@@ -68,14 +135,14 @@ def optimal(pg, cap):
                     furthest = max(distances, key=distances.get)  
                     #print(distances)
                     #print(furthest)
-                frames.remove(frames[furthest])
-                frames.append(i)
+                frames.remove(frames[furthest])#changes frames 0 to frames furthest
+                frames.append(i)#change to replaces frames furthest
                 fr_index = 0
             else:
                 frames.append(i)
 
             faults += 1
-            pg_index += 1
+        pg_index += 1
     return faults
  
     
